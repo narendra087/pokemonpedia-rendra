ec2d7e426880df0a643a735a23a87c2daf0e95a6
Narendra <personal.rendra@gmail.com>
2022-01-23 17:20:35 +0700

    fix duplicate nickname

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import '../assets/scss/Popup.scss'
 
 function Popup(props) {
   const [nickname, setNickname] = useState('')
+  const [invalid, setInvalid] = useState(false)
+
+  const myPokemon = useSelector((state) => state.pkmn.myPokemon);
 
   function handleChange(e) {
     setNickname(e.target.value)
+    setInvalid(false)
   }
 
   function closePopup(){
@@ -13,17 +18,35 @@ function Popup(props) {
   }
 
   function saveNickname(){
-    props.handleSave(nickname)
+    foundNickname()
+    if (!foundNickname()) {
+      props.handleSave(nickname)
+    } else {
+      setInvalid(true)
+    }
+  }
+
+  function foundNickname(){
+    const nick = nickname || props.defaultName
+    const foundPokemon = myPokemon.find(obj => obj.nickname === nick)
+    if (foundPokemon) {
+      return true
+    } else {
+      return false
+    }
   }
 
   const Input = () => {
     if (props.withInput) {
-      return <div>
-        <input className='form popup__form' autoFocus="autoFocus" type="text" value={nickname} onChange={handleChange}  placeholder={`default: ${props.defaultName}`} />
+      const element = 
+      <div>
+        <input className={`form popup__form ${invalid ? 'form__invalid' : ''}`} autoFocus="autoFocus" type="text" value={nickname} onChange={handleChange}  placeholder={`default: ${props.defaultName}`} />
+        {(invalid) ? (<p className='form__alert'>Nickname already exists!</p>) : ('') }
         <div className="popup__action">
           <button className='btn btn__primary' onClick={() => {saveNickname()}}>Save</button>
         </div>
       </div>
+      return element
     } else {
       return ''
     }
